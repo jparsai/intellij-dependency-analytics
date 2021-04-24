2b4d49d7473c7093cc2c757e85edf5c556e87f4d
jparsai <jparsai@redhat.com>
2021-04-24 19:20:21 +0530

    Added Run Action.

---
+++ src/main/java/org/jboss/tools/intellij/analytics/StackAnalysisAction.java
@@ -0,0 +1,29 @@
+package org.jboss.tools.intellij.analytics;
+
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class StackAnalysisAction extends AnAction {
+    private static final Logger log = Logger.getInstance(StackAnalysisAction.class);
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        log.warn("Inside PopupDialogAction:actionPerformed ");
+        log.warn("Manifest File Location= "+ event.getData(PlatformDataKeys.VIRTUAL_FILE).getPath());
+    }
+
+    @Override
+    public void update(AnActionEvent event) {
+        log.warn("Inside PopupDialogAction:update");
+        List<String> extensions = Arrays.asList("xml", "json", "txt");
+        PsiFile psiFile = event.getData(CommonDataKeys.PSI_FILE);
+        event.getPresentation().setEnabledAndVisible(extensions
+                .contains(psiFile.getFileType().getDefaultExtension()));
+    }
+}
